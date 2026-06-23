5be8dbbbc471273b5109827e312506c4a76fedf3
a6563801 <a6563801@gmail.com>
2026-06-23 21:29:43 +0800

    20260623

--- n13310020/n13310020/Formmune.cs
+++ n13310020/n13310020/Formmune.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,18 @@ namespace n13310020
         public Formmune()
         {
             InitializeComponent();
+            if (!File.Exists("OrderData.csv"))
+                File.WriteAllText("OrderData.csv", "時間,主食,配菜,飲品,甜點\n", Encoding.UTF8);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            foreach(Control c in panel1.Controls)
+            DateTime currentDataTime = DateTime.Now;
+            string formattedDataTime = currentDataTime.ToString("yyyy/MM/dd HH:mm:ss");
+
+            string food = "", sidefood = "", drink = "", dessert = "";
+
+            foreach(Control c in panel1.Controls) //主食
             {
                 if(c is CheckBox)
                 {
@@ -27,12 +35,13 @@ namespace n13310020
                     if(temp.Checked == true)
                     {
                         MessageBox.Show(temp.Text);
+                        food+=" "+temp.Text;
                     } 
 
 
                 }
             }
-            foreach (Control c in panel2.Controls)
+            foreach (Control c in panel2.Controls)//配菜
             {
                 if (c is CheckBox)
                 {
@@ -40,12 +49,13 @@ namespace n13310020
                     if (temp.Checked == true)
                     {
                         MessageBox.Show(temp.Text);
+                        sidefood+=" "+temp.Text;
                     }
 
 
                 }
             }
-            foreach (Control c in panel3.Controls)
+            foreach (Control c in panel3.Controls)//飲料
             {
                 if (c is CheckBox)
                 {
@@ -53,6 +63,7 @@ namespace n13310020
                     if (temp.Checked == true)
                     {
                         MessageBox.Show(temp.Text);
+                        drink+=" "+temp.Text;
                     }
 
 
@@ -66,12 +77,18 @@ namespace n13310020
                     if (temp.Checked == true)
                     {
                         MessageBox.Show(temp.Text);
+                        dessert+=" "+temp.Text;
                     }
 
 
                 }
             }
+            File.AppendAllText("OrderData.csv", formattedDataTime+","+food+", "+sidefood+", "+drink+", "+dessert+"\n");
+            MessageBox.Show("點餐完成,慢慢等不要催");
         }
+
+
+        
          
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
